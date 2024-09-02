a7358ea852c65348fcf945f02ef945f219619d78
Chetan Dewalkar <chetandewalkar@gmail.com>
2024-09-02 19:52:09 +0530

    second commit

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,18 @@
 const wishContainer = document.getElementById('wish-container');
 const contentContainer = document.getElementById('content-container');
+const nameInput = document.getElementById('name-input');
 
 function wish() {
-  const nameInput = document.getElementById('name-input');
   const nameSpace = document.getElementById('name-space');
 
   contentContainer.style.display = 'none';
   nameSpace.innerHTML = nameInput.value;
   wishContainer.style.display = 'flex';
+  wishContainer.className = 'animate__animated animate__zoomIn ';
 }
 
 function closeWish() {
-  wishContainer.style.display = 'none';
   contentContainer.style.display = 'flex';
+  wishContainer.style.display = 'none';
+  nameInput.value = '';
 }
