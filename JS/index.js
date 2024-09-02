a9e10c6487e79fe82f40e344a2bcedf9bef37b42
Chetan Dewalkar <chetandewalkar@gmail.com>
2024-09-02 20:33:13 +0530

    third commit

--- JS/index.js
+++ JS/index.js
@@ -5,14 +5,22 @@ const nameInput = document.getElementById('name-input');
 function wish() {
   const nameSpace = document.getElementById('name-space');
 
-  contentContainer.style.display = 'none';
-  nameSpace.innerHTML = nameInput.value;
-  wishContainer.style.display = 'flex';
-  wishContainer.className = 'animate__animated animate__zoomIn ';
+  if (nameInput.value.trim().length == 0) {
+    const message = document.getElementById('message');
+    message.innerHTML = 'Please enter name!';
+    message.style.color = 'red';
+    message.style.display = 'block';
+  } else {
+    contentContainer.style.display = 'none';
+    nameSpace.innerHTML = nameInput.value;
+    wishContainer.style.display = 'flex';
+    wishContainer.className = 'animate__animated animate__zoomIn ';
+  }
 }
 
 function closeWish() {
   contentContainer.style.display = 'flex';
   wishContainer.style.display = 'none';
   nameInput.value = '';
+  message.style.display = 'none';
 }
